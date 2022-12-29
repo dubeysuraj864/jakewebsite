0432831a398b6c82a53cb275a80e506b9a28902c
Suraj Dubey <dubeysuraj864@gmail.com>
2022-12-29 23:36:08 +0530

    final

--- src/components/Payments.jsx
+++ src/components/Payments.jsx
@@ -2,7 +2,7 @@
 function Payments() {
   
     return ( <div className="flex flex-col justify-center items-center h-min-[100vh] ">
-       <div className="py-[20vh]"> If showing blank page, then refresh the page.</div>
+       <div className="py-[20vh] text-center"> If showing blank page, then refresh the page. <br /> else wait for 10sec then refresh again.</div>
     <div id="paypal"></div>
    
     </div> );
